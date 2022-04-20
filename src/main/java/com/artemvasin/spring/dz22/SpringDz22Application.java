4387d16fcad5187b6f526b208b0896cd74d26f1e
Wasin Artem <wartem87@gmail.com>
2022-04-20 22:04:40 +0400

    Initial commit

---
+++ src/main/java/com/artemvasin/spring/dz22/SpringDz22Application.java
@@ -0,0 +1,13 @@
+package com.artemvasin.spring.dz22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDz22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDz22Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/artemvasin/spring/dz22/SpringDz22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemvasin.spring.dz22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDz22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
